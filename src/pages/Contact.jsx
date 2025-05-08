b75eb2ab9325475b79281e5cfb47aa6cb3ef796c
Karoly Hornyak <karoly.webdev@gmail.com>
2025-05-08 17:40:59 +0100

    Update

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -8,7 +8,7 @@ import ActionButton from "../components/buttons/ActionButton";
 
 export default function Contact() {
     const [captchaToken, setCaptchaToken] = useState(null);
-    const siteKey = import.meta.env.VITE_RECAPTCHA_SITE_KEY;
+    const siteKey = "6LdB4zErAAAAAIGsGkAvQl75wc7gPCHfCB1c4O-2";
 
     const handleCaptchaChange = (token) => {
         setCaptchaToken(token);
