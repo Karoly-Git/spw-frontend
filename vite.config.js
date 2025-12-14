4ecc6f45644bd2cd7edfb29927bd77b0037198a6
Karoly Hornyak <karoly.webdev@gmail.com>
2025-12-14 18:04:58 +0000

    Vite config update

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   css: {
-<<<<<<< HEAD
-    devSourcemap: true, // 👈 enables CSS source maps
-=======
-    devSourcemap: true, // This enables sourcemaps for CSS in dev
->>>>>>> 2f791f5ee143a2738b00802c2531833e9327858e
+    devSourcemap: true
   },
 })
